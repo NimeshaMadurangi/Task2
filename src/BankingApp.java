24e8e3a39008f01ced95aa190d79ec97a28f2f2d
IT21178986-Nimesha Madurangi <116791096+NimeshaMadurangi@users.noreply.github.com>
2024-02-06 17:04:28 +0530

    banking app main

--- src/BankingApp.java
+++ src/BankingApp.java
@@ -6,8 +6,45 @@ public class BankingApp {
 
         Scanner sc = new Scanner(System.in);
 
+        BankDetails bank = new BankDetails();
 
+        int n;
 
-    }
+        System.out.println("Please select what you want");
 
+        System.out.println("1. Open a new Account");
+        System.out.println("2. Show Details");
+        System.out.println("3. Deposit Money");
+        System.out.println("4. Money Withdraw");
+        System.out.println("5. Search");
+
+        System.out.print("Enter Your Need: ");
+        n = sc.nextInt();
+
+            switch (n){
+
+                case 1:
+                    bank.openAcc();
+                    System.out.println("Successfully created");
+                    break;
+                case 2:
+                    bank.showAcc();
+                    System.out.println("Thank You");
+                    break;
+                case 3:
+                    bank.deposit();
+                    System.out.println("Successfully Deposit");
+                    break;
+                case 4:
+                    bank.withdrawal();
+                    System.out.println("Thank You");
+                    break;
+                case 5:
+                    Long searchAcc = sc.nextLong();
+                    bank.search(searchAcc);
+                    System.out.println("Thank You");
+                    break;
+            }
+        }
 }
+
